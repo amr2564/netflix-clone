bd4ba3d855023d78affeb949cdb3f792b65ebb73
Ammar Ameer <ammarameeramr@gmail.com>
2020-10-26 20:38:34 +0530

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -21,11 +21,6 @@ function Nav() {
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="Netflix Logo"
       />
-      <img
-        className="nav_avatar"
-        src="https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png"
-        alt="Netflix Logo"
-      />
     </div>
   );
 }
